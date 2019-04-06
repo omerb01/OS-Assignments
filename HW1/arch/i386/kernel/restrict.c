957e0b78cf3c0366e0ba4016256c50609848cd55
Oran Nahoum <33873115+oranichu@users.noreply.github.com>
2019-04-06 16:50:22 +0300

    Checks restriction

---
+++ HW1/arch/i386/kernel/restrict.c
@@ -0,0 +1,19 @@
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
+
+asmlinkage int check_restrictions (int pid) {
+    for (int i = 0; i < current->hw1_list_size; ++i) {
+        if (current->hw1_restrictions_list[i].syscall_num == pid) {
+
+            if (current->hw1_restrictions_list[i].restriction >
+                currnet->hw1_proc_restriction_level) {
+                // ToDo: enter to log
+                return -ENOSYS;
+            }
+        }
+    }
+
+    return 0 ; // Success
+}
