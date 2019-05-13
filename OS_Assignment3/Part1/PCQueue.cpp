daae9e8bf72b20cee575a6fff411f3c7d100767d
Omer Belhasin <omerb01@gmail.com>
2019-05-13 20:42:33 +0300

    init

---
+++ OS_Assignment3/Part1/PCQueue.cpp
@@ -0,0 +1,13 @@
+#include "PCQueue.hpp"
+
+template <class T> // Add definition in PCQueue.hpp if you need this constructor
+PCQueue<T>::PCQueue(): /* Init list*/ {
+}
+
+template <class T>
+T PCQueue<T>::pop(){
+}
+
+template <class T>
+void PCQueue<T>::push(const T& item){
+}
